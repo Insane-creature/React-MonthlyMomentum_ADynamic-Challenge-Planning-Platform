e9da32e15d7240af5410d4c2293f281fcb89f1bb
Anshika Soni <anshikasoni357@gmail.com>
2024-05-24 03:55:52 +0530

    Added handle submits

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import ChallengeList from './component/ChallengeList';
 import axios from 'axios'
+import AddChallenge from './component/AddChallenge';
 
 function App() {
 
   const[challenges, setChallenge] = useState([]);
   
   useEffect(()=> {
-    const fetchChallenge = async () => {
+    fetchChallenge();
+  },[]);
+  
+  const fetchChallenge = async () => {
+    try {
       const response = await axios.get('http://localhost:8080/challenges');
       setChallenge(response.data);
-    };
+    } catch (error) {
+      console.log("Error fetching the challenges", error);
+    }
+    
+  };
+
+  const handleChallengeAdded = () => {
     fetchChallenge();
-  },[])
+  }
 
   return (
     <div className="App">
       <h1>Monthly Planner</h1>
+      <AddChallenge onChallengeAdded = {handleChallengeAdded}/>
       <ChallengeList challenges={challenges}/>
     </div>
   );
---
+++ src/component/AddChallenge.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useState } from "react";
+
+function AddChallenge({onChallengeAdded}){
+
+    const [month, setMonth] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:8080/challenges', {month, description});
+            setMonth('');
+            setDescription('');
+            onChallengeAdded();
+        } catch (error) {
+            console.log("Error addding the challenges", error);
+        }
+    }
+
+    return(
+        
+        <div>
+            <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="month">Month</label>
+                <input type="text" id= "month" value={month} onChange={(e) =>setMonth(e.target.value)} required></input>
+            </div>
+            <div>
+                <label htmlFor="description">Description</label>
+                <textarea id= "description" value={description} onChange={(e) =>setDescription(e.target.value)} required></textarea>
+            </div>
+            <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default AddChallenge;
